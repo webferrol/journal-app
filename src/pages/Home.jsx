c8a94c37fbb53562189cc49605e79c0c20f295ec
webferrol <xurxo@webferrol.com>
2023-07-12 10:48:39 +0200

    Pruebas con la rama develop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
+import { NothingSelectedView } from "../views/NothingSelectedView";
+
 export function Home () {
   return (
-    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde labore nostrum eligendi repellat nam nobis alias harum neque error? Beatae voluptatem provident non vitae pariatur odio facilis minus, aliquam magnam! </div>
+    <div>
+      <NothingSelectedView />
+    </div>
   )
 }
